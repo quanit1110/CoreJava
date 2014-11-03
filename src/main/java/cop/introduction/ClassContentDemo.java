683e2e4195b3f3a1253140aaef25bd7ca28ca5dd
Mital <mjanimahant@gmail.com>
2014-11-02 23:26:26 -0800

    Demonstrating Legal Class Content

---
+++ src/main/java/cop/introduction/ClassContentDemo.java
@@ -0,0 +1,24 @@
+package cop.introduction;
+
+/**
+ * Created by Mital Jani on 11/2/14.
+ */
+public class ClassContentDemo {
+    //Class can only contain class members
+
+    //Static variable
+    static int staticVariable;
+
+    //Instance variable
+    int instanceVariable = 0;
+
+    //Static Method
+    public static void printMe() {
+        System.out.println("This is Static Method");
+    }
+
+    //Instance Method
+    public void printInstanceMethod() {
+        System.out.println("Wooooohoooo I am in Instance Method");
+    }
+}
