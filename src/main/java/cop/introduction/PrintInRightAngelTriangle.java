74a8185689d5e4ec6740a38768d0dbe3953575de
Mital <mjanimahant@gmail.com>
2014-11-04 08:34:06 -0800

    Demonstrating for loop and printing name and symbol in right angled triangle.

--- src/main/java/cop/introduction/PrintInRightAngelTriangle.java
+++ src/main/java/cop/introduction/PrintInRightAngelTriangle.java
@@ -5,33 +5,34 @@ package cop.introduction;
  */
 public class PrintInRightAngelTriangle {
 
-  public void printName (String name) {
-      String input = name;
-      String newInput = "";
-      for (int i = 0; i < input.length(); i++) {
-          char c = input.charAt(i);
-          newInput+= c;
-          System.out.println(newInput);
-      }
-  }
+    public void printName(String name) {
+        String input = name;
+        String newInput = "";
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            newInput += c;
+            System.out.println(newInput);
+        }
+    }
 
-  public void printSymbol (int number){
-      int n = number;
-      String input = "";
-      for (int i = 0; i < number; i++){
-         input += "*";
-          System.out.println (input);
-      }
-  }
-      public static void main (String[]args){
-          PrintInRightAngelTriangle sd = new PrintInRightAngelTriangle();
-          System.out.println ("Printing Name in Triangle");
-          sd.printName("Mital");
-          System.out.println ("\nPrinting Symbols in Triangle");
-          sd.printSymbol(5);
+    public void printSymbol(int number) {
+        int n = number;
+        String input = "";
+        for (int i = 0; i < number; i++) {
+            input += "*";
+            System.out.println(input);
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintInRightAngelTriangle sd = new PrintInRightAngelTriangle();
+        System.out.println("Printing Name in Triangle");
+        sd.printName("Mital");
+        System.out.println("\nPrinting Symbols in Triangle");
+        sd.printSymbol(5);
 
     }
 
 
-    }
+}
 
